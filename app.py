22aa7d1f934e07abfd0a72a622b32114cbe8c003
Sriveda <srivedagundu@gmail.com>
2026-03-28 05:40:56 -0400

    booksummarizerupdated

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from openai import OpenAI
-from utils import read_text_from_file
+from utils import read_text_from_file, chunk_text
 client = OpenAI()
 
 book_text = read_text_from_file(r"C:\Users\12269\Desktop\openAI_project\book.txt")
-response = client.responses.create(
-  model="gpt-5",
-  input = f"Summarize the book in simple words to a 10 year old kid: {book_text}"
+chunks = chunk_text(book_text, 100)
 
-)  
+print("no.of chunks:" , len(chunks))
 
-print(response.output_text)
+#output =[]
+for chunk in chunks:
+    chunk_summary = client.responses.create(
+        model="gpt-5.4",
+        input = f"Summarize this text: {chunk}"
+    )
+    #output.append(chunk_summary)
+    print(chunk_summary.output_text, end="==============================")
+    
+
+
+# for i in range(0, len(output)):
+#     print("chunk_number:",i+1, "Summary:", output[i], end=" ")    
+
+
+# response = client.responses.create(
+#   model="gpt-5",
+#   input = f"Summarize the book in simple words to a 10 year old kid: {book_text}"
+
+# )  
+
--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
 from pathlib import Path
+
+# chunking logic
+# chunk_size = 1000
+
+def chunk_text(text: str, chunk_size: int= 1000) -> list[str]:
+    words = text.split(' ')
+    chunks=[]
+    i=0
+    while i< len(words):
+        chunk = words[i: i+chunk_size-1]
+        chunks.append(chunk)
+        i += chunk_size
+    return chunks
 def read_text_from_file(file_path: str) -> str:
     path = Path(file_path)
     with path.open('r', encoding="utf-8") as file:
